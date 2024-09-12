42a1926ba931b7c170b93a8319345a5b2b0b2ef8
Wander Amaral <wanderamaral013@gmail.com>
2024-09-12 15:04:54 -0300

    commit

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+import { Plus } from 'lucide-react'
+
+export function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="start" />
+      <p className="text-zinc-300 leadind-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <button
+        className="flex items-center gap-2 px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 text-sm font-medium tracking-tight hover:bg-violet-600"
+        type="button"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  )
+}
