9b77dbaa89f20d41585a97946608b2ce9e79bf15
Wander Amaral <wanderamaral013@gmail.com>
2024-09-13 15:59:01 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from 'react'
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-//import { EmptyGoal } from './components/empty-goal'
+import { EmptyGoal } from './components/empty-goal'
+
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
 
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setSummary(data.summary)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <Dialog>
-      {/* <EmptyGoal /> */}
-      <Summary />
+      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoal />}
+
       <CreateGoal />
     </Dialog>
   )
